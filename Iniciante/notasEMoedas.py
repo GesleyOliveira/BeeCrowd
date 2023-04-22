4c2f2307b82029f124c03707e9d4ae9a6e7fdde6
Gesley de Oliveira Rosa <geeh.oliveira@hotmail.com>
2023-04-22 01:37:03 -0300

    Update notasEMoedas.py

--- Iniciante/notasEMoedas.py
+++ Iniciante/notasEMoedas.py
@@ -9,6 +9,38 @@ Imprima a quantidade mínima de notas e moedas necessárias para trocar o valor 
 Obs: Utilize ponto (.) para separar a parte decimal."""
 
 valor = float(input())
-notas = [100, 50, 20, 10, 5, 2]
-moedas = [1, 0.50, 0.25, 0.10, 0.05 e 0.01]
 
+notas = [100, 50, 20, 10, 5, 2]
+moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
+
+print("NOTAS:")
+for nota in notas:
+    numero_nota = int(valor / nota)
+    print(f"{numero_nota} nota(s) de R$ {nota:.2f}")
+    valor -= numero_nota * nota
+    
+print("MOEDAS:")
+for moeda in moedas:
+    numero_moedas = int(round(valor, 2) / moeda)
+    print(f"{numero_moedas} nota(s) de R$ {moeda:.2f}")
+    valor -= numero_moedas * moeda
+    
+    
+"""Em Python 3"""
+
+valor = float(input())
+
+notas = [100, 50, 20, 10, 5, 2]
+moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
+
+print("NOTAS:")
+for nota in notas:
+    numero_nota = int(valor / nota)
+    print("{} nota(s) de R$ {:.2f}".format(numero_nota, nota))
+    valor -= numero_nota * nota
+    
+print("MOEDAS:")
+for moeda in moedas:
+    numero_moedas = int(round(valor, 2) / moeda)
+    print("{} moeda(s) de R$ {:.2f}".format(numero_moedas, moeda))
+    valor -= numero_moedas * moeda
